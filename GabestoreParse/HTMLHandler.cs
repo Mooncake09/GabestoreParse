2cc71cf1c735521f0b7050813ae4dfe52a83d763
mooncake09 <zahich96@gmail.com>
2021-09-07 22:05:27 +0300

    Удален метод с регулярными выражениями

--- GabestoreParse/HTMLHandler.cs
+++ GabestoreParse/HTMLHandler.cs
@@ -37,20 +37,5 @@ namespace GabestoreParse
             Console.WriteLine(genre);
             return new List<Game>();
         }
-        public static void TestRegexp()
-        {
-            string s = "Бык тупогуб, тупогубенький бычок, у быка губа бела была тупа";
-            Regex regex = new Regex(@"туп(\w*)");
-            MatchCollection matches = regex.Matches(s);
-            if (matches.Count > 0)
-            {
-                foreach (Match match in matches)
-                    Console.WriteLine(match.Value);
-            }
-            else
-            {
-                Console.WriteLine("Совпадений не найдено");
-            }
-        }
     }
 }
